aac48fdd8a2d53e1b4dd99df1111c21394f8be71
Matthias Klumpp <matthias@tenstral.net>
2020-05-23 00:07:11 +0200

    Add package structure

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from debspawn import __appname__, __version__
+from setuptools import setup
+
+packages = [
+    'edlio',
+]
+
+setup(
+    name=__appname__,
+    version=__version__,
+    author="Matthias Klumpp",
+    author_email="matthias@tenstral.net",
+    description='Module to work with data in an Experiment Directory Layout (EDL) structure',
+    license="LGPL-3.0+",
+    url="https://edl.readthedocs.io/",
+
+    python_requires='>=3.5',
+    platforms=['any'],
+
+    packages=packages
+)
