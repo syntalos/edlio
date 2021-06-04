4fb9c434b17f2589cd56ddfcb6afdec41e2e4197
Matthias Klumpp <matthias@tenstral.net>
2021-06-04 03:03:15 +0200

    trivial: Add reference to long description text

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setup(
     description='Module to work with data in an Experiment Directory Layout (EDL) structure',
     license="LGPL-3.0+",
     url="https://edl.readthedocs.io/",
+    long_description=open(os.path.join(source_root, 'README.md')).read(),
+    long_description_content_type='text/markdown',
     
     install_requires=[
         'toml>=0.10',
