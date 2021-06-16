100d5f6458d76800adaf8d5dd19dec507b2936e9
Matthias Klumpp <matthias@tenstral.net>
2021-06-16 22:49:56 +0200

    Add small linter helper script
    For users who want to lint everything quickly locally.

---
+++ lint-all.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+echo "Flake8"
+flake8 ./edlio ./tests --show-source --statistics
+echo "OK"
+
+echo "Pylint"
+python -m pylint -f colorized ./edlio ./tests
+python -m pylint -f colorized setup.py
+echo "OK"
+
+echo "MyPy"
+mypy --pretty edlio
+echo "OK"
