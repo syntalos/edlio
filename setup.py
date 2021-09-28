95c4fe537e0717d511119b421b0b7040e89b2e2b
Matthias Klumpp <matthias@tenstral.net>
2021-09-28 19:19:22 +0200

    Make sure the wheel build always contains dependency data
    Depending on how the wheel was built, it either had dependency data
    before, or didn't have it. Python packaging is a mess.
    The pyproject.toml file is now the canonical source of all iformation on
    eldio, and no matter whether the wheel is built via setup.py or Flit, it
    will always have comparable information.
    Only building via Flit should be used for PyPI uploads though.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import pathlib
+import toml
 from edlio import __appname__, __version__
 from setuptools import setup
 
@@ -13,24 +15,21 @@ packages = [
     'edlio.dataio',
 ]
 
+pyproject_data = toml.loads(pathlib.Path('pyproject.toml').read_text())
+
 setup(
     name=__appname__,
     version=__version__,
     author="Matthias Klumpp",
     author_email="matthias@tenstral.net",
     description='Module to work with data in an Experiment Directory Layout (EDL) structure',
-    license="LGPL-3.0+",
     url="https://edl.readthedocs.io/",
+    license=pyproject_data['project']['license']['text'],
     long_description=open(os.path.join(source_root, 'README.md')).read(),
     long_description_content_type='text/markdown',
 
-    install_requires=[
-        'toml>=0.10',
-        'numpy>=1.17',
-        'xxhash>=2.0',
-        'Pint>=0.10'
-    ],
-    python_requires='>=3.8',
+    install_requires=pyproject_data['project']['dependencies'],
+    python_requires=pyproject_data['project']['requires-python'],
     platforms=['any'],
 
     packages=packages
